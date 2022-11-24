c4643a51ee9fd7e084fbec58951e7282070b1f33
Mikhail Serebryakov <soraenjoyer@mail.ru>
2022-11-24 19:30:47 +0500

    Hello, world!

---
+++ src/main.c
@@ -0,0 +1,19 @@
+/**
+ * src/main.c
+ *
+ * Description: Main script of the program
+ * Author(s): Mikhail Serebryakov <soraenjoyer@mail.ru>
+ * Changelog: 2022/11/24 19:30 - Script was created.
+ * License: Public domain
+ */
+
+#include <stdio.h>
+
+/**
+ * Description: Main function of the program
+ **/
+int main()
+{
+	printf("Hello, %s!\n", "world");
+	return 0;
+}
